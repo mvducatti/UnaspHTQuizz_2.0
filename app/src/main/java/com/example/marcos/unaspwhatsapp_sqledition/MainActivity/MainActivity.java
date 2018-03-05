f580718169e21a858a13d2aa9d4fe8d0c5ef1b5d
mvducatti <mvducatti@gmail.com>
2018-03-05 17:27:59 -0300

    first commit

--- app/src/main/java/com/example/marcos/unaspwhatsapp_sqledition/MainActivity/MainActivity.java
+++ app/src/main/java/com/example/marcos/unaspwhatsapp_sqledition/MainActivity/MainActivity.java
@@ -3,22 +3,16 @@ package com.example.marcos.unaspwhatsapp_sqledition.MainActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.example.marcos.unaspwhatsapp_sqledition.Adapter.NewsRecyclerAdapter;
-import com.example.marcos.unaspwhatsapp_sqledition.Adapter.UsersRecyclerAdapter;
 import com.example.marcos.unaspwhatsapp_sqledition.Database.DBNoticias;
-import com.example.marcos.unaspwhatsapp_sqledition.Database.DBUsuario;
-import com.example.marcos.unaspwhatsapp_sqledition.Main2Activity;
 import com.example.marcos.unaspwhatsapp_sqledition.Model.Noticia;
-import com.example.marcos.unaspwhatsapp_sqledition.Model.User;
 import com.example.marcos.unaspwhatsapp_sqledition.R;
 
 import java.util.ArrayList;
